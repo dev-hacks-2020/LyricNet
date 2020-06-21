149cdf3ef29214cf2a088246e7cb7afd7ddbce28
Harsh-Topi <topiwalah@hotmail.com>
2020-06-20 21:53:42 -0400

    replaced browserrouter with hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,17 @@ import './assets/css/main.css';
 import Home from './components/Home';
 import Generate from './components/Generate';
 import Question from './components/Question';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/generate" component={Generate} />
         <Route path="/question" component={Question} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
