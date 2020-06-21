4c3fc84545418d6d78d803bd4eae06a9ace96c76
ladanuzhna <54150144+ladanuzhna@users.noreply.github.com>
2020-06-21 11:25:50 -0500

    Removed hardcoded singers. Made a function.
    Songs can be now written to a file  or returned as a text corpus - can specify in the function.

--- API/genius.py
+++ API/genius.py
@@ -2,17 +2,21 @@ from dotenv import load_dotenv
 import lyricsgenius
 import os
 
-load_dotenv()
-CLIENT_ACCESS_TOKEN = os.getenv('CLIENT_ACCESS_TOKEN')
+def get_songs(singer, write_to_file = False):
+    load_dotenv()
+    CLIENT_ACCESS_TOKEN = os.getenv('CLIENT_ACCESS_TOKEN')
 
-genius = lyricsgenius.Genius(CLIENT_ACCESS_TOKEN)
-genius.skip_non_songs = True
-genius.remove_section_headers = True
-genius.excluded_terms = ["(Remix)", "(Live)", "(Speech)"]
-artist = genius.search_artist('Taylor Swift', max_songs=500)
-
-with open('Taylor_Swift_2.txt', 'x') as text:
-    for song in artist.songs:
-        text.write(song.lyrics)
-        text.write(
-            '\n-----------------------------------------------------------\n')
+    genius = lyricsgenius.Genius(CLIENT_ACCESS_TOKEN)
+    genius.skip_non_songs = True
+    genius.remove_section_headers = True
+    genius.excluded_terms = ["(Remix)", "(Live)", "(Speech)"]
+    artist = genius.search_artist(singer, max_songs=500)
+    
+    if write_to_file:
+        with open('Taylor_Swift_2.txt', 'x') as text:
+            for song in artist.songs:
+                text.write(song.lyrics)
+                text.write(
+                    '\n-----------------------------------------------------------\n')
+    else:
+        return artist.songs
